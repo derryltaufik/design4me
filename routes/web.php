8eb35c08228ad15e97871441a713d71f63dddd9c
derryltaufik <derryl@users.noreply.github.com>
2020-12-15 03:07:49 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,25 @@ Auth::routes();
 
 Route::get('/', 'DesignController@home')->name('home');
 
+Route::middleware('auth')->group(function(){
+    Route::get('/designs', 'DesignController@index')->name('designs.index');
+    Route::get('/designs/create', 'DesignController@create')->name('designs.create');
+    Route::post('/designs', 'DesignController@store')->name('designs.store');
 
-Route::get('/designs', 'DesignController@index')->name('designs.index');
-Route::get('/designs/create', 'DesignController@create')->name('designs.create');
-Route::post('/designs', 'DesignController@store')->name('designs.store');
+    Route::get('/designs/{design}', 'DesignController@show')->name('designs.show');
 
-Route::get('/designs/{design}', 'DesignController@show')->name('designs.show');
+    Route::get('/designs/{design}/edit', 'DesignController@edit')->name('designs.edit');
+    Route::put('/designs/{design}', 'DesignController@update')->name('designs.update');
 
-Route::get('/designs/{design}/edit', 'DesignController@edit')->name('designs.edit');
-Route::put('/designs/{design}', 'DesignController@update')->name('designs.update');
+    Route::delete('/designs/{design}', 'DesignController@destroy')->name('designs.destroy');
 
-Route::delete('/designs/{design}', 'DesignController@destroy')->name('designs.destroy');
+    Route::get('/carts/', 'CartController@index')->name('carts.index');
+    Route::post('/carts/{design}', 'CartController@addToCart')->name('carts.atc');
+    Route::put('/carts/{design}', 'CartController@update')->name('carts.update');
 
-Route::get('/carts/', 'CartController@index')->name('carts.index');
-Route::post('/carts/{design}', 'CartController@addToCart')->name('carts.atc');
-Route::put('/carts/{design}', 'CartController@update')->name('carts.update');
+    Route::get('/transactions', 'TransactionController@index')->name('transactions.index');
+    Route::post('/transactions', 'TransactionController@create')->name('transactions.create');
+    Route::get('/transactions/{transaction}', 'TransactionController@show')->name('transactions.show');
+});
 
-Route::get('/transactions', 'TransactionController@index')->name('transactions.index');
-Route::post('/transactions', 'TransactionController@create')->name('transactions.create');
-Route::get('/transactions/{transaction}', 'TransactionController@show')->name('transactions.show');
+
